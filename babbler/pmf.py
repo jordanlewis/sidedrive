eb6b52f864a42a53493c8ed314f451d240c08281
George Davis <george@knewton.com>
2014-07-25 18:38:00 -0400

    pmf code working

--- babbler/pmf.py
+++ babbler/pmf.py
@@ -1,4 +1,6 @@
 # pmf - implementations of frequency table pmf functions
+import numpy as np
+from random import random
 
 class PMFArray :
 	"""
@@ -6,22 +8,82 @@ class PMFArray :
 	calculations.
 	"""
 	def __init__ (self) :
-		pass
+		self.k = 0
+		self.idx = {}
+		self.outcomes = []
+		self.counts = np.array([])
+		self.cumsum = np.array([])
+		self.total = 0
+
+		# flags whether cumsum needs recomputation
+		self._dirty = False
+
+	def __str__ (self) :
+		return str([(self.outcomes[i], self.counts[i]) for i in xrange(self.k)])
+
+	def _clean (self) :
+		# updates cumsum if necessary; called before cumsum is used
+		if self._dirty :
+			self.cumsum = np.cumsum(self.counts)
+			self._dirty = False
 
 	def count (self, outcome, n = 1) :
 		# increment frequency for given outcome by n
-		pass
+		idx = self.idx.get(outcome)
+		if idx is None :
+			idx = self.k
+			self.idx[outcome] = idx
+			self.k += 1
+			self.outcomes.append(outcome)
+			self.counts = np.append(self.counts, 0)
 
-	def inv_cdf (self, p) :
-		# get the outcome at a given probability
-		pass
+		self.counts[idx] += n
+		self.total += n
+		self._dirty = True
 
-	def sample (self, context) :
-		pass
+	def inv_cmf (self, p) :
+		if p < 0 or p > 1 :
+			raise Exception("p outside [0,1]: %f"% p)
+		self._clean()
 
-	def get_count (self, outcome = None) :
-		# return total count or count of given outcome
-		pass
+		v = p * self.total
+		idx = np.searchsorted(self.cumsum, v)
+		return self.outcomes[idx]
+
+	def get_p (self, outcome) :
+		idx = self.idx.get(outcome)
+		if idx is None :
+			raise Exception("Unknown outcome: %s"%outcome)
+		return self.counts[idx] / self.total
+
+	def range_condition (self, p0, p1) :
+		if p0 < 0 or p0 > 1 :
+			raise Exception("p0 outside [0,1]: %f" % p0)
+		if p1 < 0 or p1 > 1 :
+			raise Exception("p1 outside [0,1]: %f" % p1)
+		if p1 <= p0 :
+			raise Exception("p0 >= p1: %s" % (p0, p1))
+		self._clean()
+
+		# cuts in frequency space
+		v0 = self.total * p0
+		v1 = self.total * p1
+
+		# iterate through touched values and add to new PMF
+		cpmf = PMFArray()
+		i0, i1 = np.searchsorted(self.cumsum, (v0,v1))
+		for i in xrange(i0, i1+1) :
+			vi = min(self.cumsum[i], v1)
+			cpmf.count(self.outcomes[i], vi - v0)
+			v0 = vi
+	
+		return cpmf
+
+	def sample (self) :
+		if self.total == 0 :
+			return None
+		p = random()
+		return self.inv_cmf(p)
 
 
 class PMFTree :
@@ -30,20 +92,4 @@ class PMFTree :
 	frequency count, allowing fast online adjustments and inverse CDF lookups 
 	(and thus sampling).
 	"""
-	def __init__ (self) :
-		pass
-
-	def count (self, outcome, n = 1) :
-		# increment frequency for given outcome by n
-		pass
-
-	def inv_cdf (self, p) :
-		# get the outcome at a given probability
-		pass
-
-	def sample (self, context) :
-		pass
-
-	def get_count (self, outcome = None) :
-		# return total count or count of given outcome
-		pass
+	pass
